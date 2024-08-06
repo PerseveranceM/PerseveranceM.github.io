2270caa2983fb7dccdd52aed2d8b0eca0f3dec9d
Perseverance Molefe <Perseverance.u7@outlook.com>
2024-08-06 10:10:30 +0200

    fixed errors

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 document.getElementById("resume").addEventListener("click", function () {
   window.open("./Documents/Perseverance-Molefe-CV.pdf", "_blank");
 });
+
+document.querySelectorAll(".nav-links a").forEach((link) => {
+  link.addEventListener("click", () => {
+    document.getElementById("check").checked = false;
+  });
+});
